25389bf69f530292c73aedc25e9c9c6c4c53f241
Onuh <talk2onuh@gmail.com>
2022-12-07 10:36:38 +0000

    istalled toastr and datatables

--- resources/views/layouts/admin-head.blade.php
+++ resources/views/layouts/admin-head.blade.php
@@ -7,3 +7,6 @@
     <link href="{{ asset('admin/vendor/fonts/circular-std/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('admin/libs/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets/gen/css/datatable.buttons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/gen/css/dataTables.bootstrap4.min.css') }}">
